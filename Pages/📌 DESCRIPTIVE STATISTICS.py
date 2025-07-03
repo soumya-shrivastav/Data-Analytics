c5194814e024392a02e0aa079c5dd1be1074422c
Vivek Sharma <156570752+vivek1m@users.noreply.github.com>
2025-07-03 20:35:37 +0530

    Update 📌 DESCRIPTIVE STATISTICS.py

--- Pages/📌 DESCRIPTIVE STATISTICS.py
+++ Pages/📌 DESCRIPTIVE STATISTICS.py
@@ -39,4 +39,37 @@ with tab2.caption("SALES BY PERCENTILES"):
  with c5:
    st.info('Percentile 0 %', icon="⏱")
    st.metric(label='USD', value=f"{np.percentile(df['SALES'], 0):,.2f}")
+def ad():
+ theme_plotly = None # None or streamlit
+ column=st.sidebar.selectbox('select a column',['COUNTRY','PRODUCTLINE','CITY'])
+ type_of_column=st.sidebar.radio("What kind of analysis",['Categorical','Numerical'])
+
+ c1,c2=st.columns([2,1])
+ with c1:
+   if type_of_column=='Categorical':
+    dist=pd.DataFrame(df[column].value_counts())
+    fig=px.bar(dist, title='CATEGORIES BY FREQUENCY',orientation="v")
+    fig.update_layout(
+      legend_title=None,
+        xaxis_title="Observation",
+        plot_bgcolor='rgba(0, 0, 0, 0)',  # Set plot background color to transparent
+        paper_bgcolor='rgba(0, 0, 0, 0)',  # Set paper background color to transparent
+        xaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show x-axis grid and set its color
+        yaxis=dict(showgrid=True, gridcolor='#cecdcd'),  # Show y-axis grid and set its color
+        font=dict(color='#cecdcd'),  # Set text color to black
+        yaxis_title='Count')
+    st.plotly_chart(fig,use_container_width=True)
+    
+   else:
+    st.subheader("NUMBER SUMMARY ")
+    st.dataframe(df['SALES'].describe(),use_container_width=True)
+
+ with c2:
+   st.subheader("Total Sales")
+   st.metric(label='USD', value=f"{np.sum(df['SALES'], 0):,.2f}",help="sum",delta=np.average(df.SALES),
+    delta_color="inverse")
+style_metric_cards(background_color="#FFFFFF",border_left_color="#686664",border_color="#000000",box_shadow="#F71938")
+  
+    
+ad()
 
